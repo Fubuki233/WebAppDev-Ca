37ebf0c534f3c0d7033f4d75a8713771d98f3674
b-chan0505 <e1505289@u.nus.edu>
2025-10-04 07:12:56 +0800

    created new spring boot app Aori
    used the standard naming convention for creating spring boot project: reversed domain name (sg.com.aori)
    - Java version: 21
    - Spring Boot version: 3.5.6
    - dependencies added: Spring Boot DevTools, Validation, Spring Data JPA, H2 Database, MySQL Driver, Thymeleaf, Spring Web, Spring Session for JDBC
    
    Co-Authored-By: rui634850-maker <231616751+rui634850-maker@users.noreply.github.com>
    Co-Authored-By: Fubuki233 <49643049+Fubuki233@users.noreply.github.com>
    Co-Authored-By: JiangYibai666 <231596363+JiangYibai666@users.noreply.github.com>
    Co-Authored-By: Simonmensi <167080242+Simonmensi@users.noreply.github.com>
    Co-Authored-By: xiaxiaobo2018 <231616501+xiaxiaobo2018@users.noreply.github.com>
    Co-Authored-By: z030511tnt-pixel <232289053+z030511tnt-pixel@users.noreply.github.com>

---
+++ aori/src/main/java/sg/com/aori/AoriApplication.java
@@ -0,0 +1,13 @@
+package sg.com.aori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoriApplication.class, args);
+	}
+
+}
---
+++ aori/src/test/java/sg/com/aori/AoriApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.aori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
