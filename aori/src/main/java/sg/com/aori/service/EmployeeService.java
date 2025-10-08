7e9e2a75fb91433d29902de0d8e33b997fceeb6e
xiaxiaobo2018 <xia.xiaobo2018@gmail.com>
2025-10-08 09:23:02 +0800

    EmployeeService

--- aori/src/main/java/sg/com/aori/service/EmployeeService.java
+++ aori/src/main/java/sg/com/aori/service/EmployeeService.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 import sg.com.aori.model.Employee;
 
+
+/**
+ * Service for Employee entity.
+ *
+ * @author xiaobo
+ * @date 2025-10-07
+ * @version 1.0
+ */
+
 public interface EmployeeService {
     Employee createEmployee(Employee employee);
 
