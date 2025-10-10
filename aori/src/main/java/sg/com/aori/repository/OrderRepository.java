e8f1d3f53b17dd93df3a3415047e7203ee7a1348
xiaxiaobo2018 <xia.xiaobo2018@gmail.com>
2025-10-10 15:43:59 +0800

    update OrderRepository

--- aori/src/main/java/sg/com/aori/repository/OrderRepository.java
+++ aori/src/main/java/sg/com/aori/repository/OrderRepository.java
@@ -63,7 +63,6 @@ public interface OrderRepository extends JpaRepository<Orders, String> {
          * Finds a specific OrderItem by its ID and ensures it belongs to the given
          * user.
          */
-<<<<<<< Updated upstream
 
         @Query("SELECT oi FROM OrderItem oi JOIN oi.order o WHERE oi.orderItemId = :itemId AND o.customerId = :userId")
         Optional<OrderItem> findOrderItemByItemIdAndUserId(
@@ -81,28 +80,4 @@ public interface OrderRepository extends JpaRepository<Orders, String> {
         // for product review
         Optional<Orders> findByOrderIdAndCustomerId(String orderId, String customerId);
 
-=======
-        // Optional<OrderItem> findOrderItemByItemIdAndUserId(String orderItemId, String
-        // userId);
-        Optional<OrderItem> findByIdAndOrder_User_Id(String orderItemId, String userId);
-        
-        /* @author Derek
-         * @date 2025-10-08
-         * @version 1.0
-         * */
-        
-        //for product review
-        Optional<Orders> findByOrderIdAndCustomerId(String orderId, String customerId);
-        
-        
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
