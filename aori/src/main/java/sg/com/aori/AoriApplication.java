14ab150364a774961820734a352521dfe0909755
b-chan0505 <e1505289@u.nus.edu>
2025-10-15 12:28:45 +0800

    updated config of AoriApplication.java
    The most robust solution is to disable URL rewriting for session tracking in Spring Boot. This forces the application to rely only on cookies, which is the standard and more secure practice. It also simplifies your filter logic as you won't have to worry about ;jsessionid in your paths.

--- aori/src/main/java/sg/com/aori/AoriApplication.java
+++ aori/src/main/java/sg/com/aori/AoriApplication.java
@@ -2,12 +2,21 @@ package sg.com.aori;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.SessionTrackingMode;
+import java.util.Collections;
 
 @SpringBootApplication
-public class AoriApplication {
+public class AoriApplication implements ServletContextInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AoriApplication.class, args);
 	}
 
+	@Override
+	public void onStartup(ServletContext servletContext) {
+		// Disable URL-based session tracking
+		servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
+	}
 }
